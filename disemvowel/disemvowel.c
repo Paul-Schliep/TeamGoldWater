cc3505090ce8798cb8b64f4f6133287d88c5df8e
Paul Schliep <schli202@deathstar.morris.umn.edu>
2013-09-26 14:52:58 -0600

    Working on disemvowel

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,40 @@
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
 #include "disemvowel.h"
 
 char* disemvowel(char* str) {
-  return "";
+  int i, len;
+  len = strlen(str); 
+  char* cloneStr = calloc(len, sizeof(char));
+  char vowels[11] = "aeiouAEIOU"; // 11 for including \0
+  for (i = 0; i < len; ++i) {
+    cloneStr[i] = str[i];
+  }
+  //for (i = 0; i < len; ++i) {
+  //  if (strchr(vowels, cloneStr[i]) != NULL) {
+  //    // check for null char
+  //    shiftLeft(cloneStr, i);
+  //    i = i-1;
+  //  }
+  //}
+  i = 0;
+  while(i < len){
+    if (strchr(vowels, cloneStr[i]) != NULL) {
+      // check for null char
+      shiftLeft(cloneStr, i);
+      //i = i-1;
+    } 
+    i++;
+  } 
+  
+  return cloneStr;
+}
+
+void shiftLeft(char* str, int index) {
+  int i;
+  for(i = index; i <= strlen(str); ++i) {
+    str[i] = str[i + 1];
+  }
+
 }
--- disemvowel/disemvowel.h
+++ disemvowel/disemvowel.h
@@ -2,5 +2,5 @@
 #define DISEMVOWEL_H_GUARD
 
 char* disemvowel(char* str);
-
+void shiftLeft(char* str, int index);
 #endif
